9489ed58c84bd8c0127459afa17381a559636e32
trym12 <158284866+trym12@users.noreply.github.com>
2024-06-06 12:36:45 +0200

    Add files via upload

---
+++ js-oppgaver/js_Scriptene/variabel.js
@@ -0,0 +1,11 @@
+let a = 24
+let b = 21
+let c = 18
+console.log(x= a + b + c)
+alder.innerHTML = (x= a + b + c)
+function x(){
+    document.getElementById("alder")
+    //document.getElementById("alder").innerHTML = "a + b +c";
+}
+/*var x = a + b + c;
+document.getElementById("alder").innerHTML = x;*/
